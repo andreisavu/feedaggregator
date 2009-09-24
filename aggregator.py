fdd72b4bd2ea47aba90d271cd2cb1d15b575168e
Andrei Savu <savu.andrei@gmail.com>
2009-09-24 18:10:26 +0300

    added the categories parameter to opml loaded and full refresh cmd

--- aggregator.py
+++ aggregator.py
@@ -28,9 +28,9 @@ def dispatch(opts, args):
     elif opts.feed is not None:
         aggregate_feed(client, opts.feed, opts.cat or "")
     elif opts.refresh_feeds is True:
-        refresh_feeds(client)
+        refresh_feeds(client, opts.cat or "")
     elif opts.opml is not None:
-        aggregate_opml(client, opts.opml)
+        aggregate_opml(client, opts.opml, opt.cat or "")
 
 def parse_cli():
     """ Setup CLI parser and use it """
@@ -41,13 +41,15 @@ def parse_cli():
     parser.add_option('', '--resetdb', action="store_true",
         dest="resetdb", default=False, help="reset database")
     parser.add_option('-r', '--refresh-feeds', action="store_true",
-        dest="refresh_feeds", default=False, help="refresh all feeds")
+        dest="refresh_feeds", default=False, help="refresh all feeds or just a category")
     parser.add_option('', '--opml', dest="opml", 
         help="aggregate feeds from opml FILE", metavar="FILE")
     parser.add_option('-f', '--feed', dest="feed",
         help="parse FEED and add to index", metavar="FEED")
-    parser.add_option('-c', '--cat', dest="cat",
+    parser.add_option('', '--cat', dest="cat",
         help="feed CATEGORIES. csv format in quotes", metavar="CATEGORIES")
+    parser.add_option('', '--hours', dest="hours",
+        help="how many HOURS back in time", metavar="HOURS")
     parser.add_option('-w', '--webui', action="store_true",
         dest="webui", default=False, help="start simple web interface")
     
@@ -111,20 +113,31 @@ def attach_connection(thread):
     thread.hbase = get_hbase_client()
     return thread
 
-def refresh_feeds(client):
+def any_in(needles, haystack):
+    for x in needles:
+        if x in haystack: return True
+    return False
+
+def parse_categories(cats):
+    return [x.strip() for x in cats.split(',')]
+
+def refresh_feeds(client, allowed_categs):
     """
     Refresh all feeds found in the database using a pool of threads. 
     """
     log.info('Starting to refresh all feeds')
     scanner = db.Scanner(client, 'Feeds', ['Meta:'])
 
-    pool = ThreadPool(50, thread_init=attach_connection) 
+    allowed_categs = parse_categories(allowed_categs)
+    pool = ThreadPool(20, thread_init=attach_connection) 
     for row in scanner:
         feed, categs = row.row, row.columns['Meta:categs'].value
+        if not any_in(parse_categories(categs), allowed_categs):
+            continue
         pool.queueTask(lambda worker, p:aggregate_feed(worker.hbase, *p), (feed, categs))
     pool.joinAll()
 
-def aggregate_opml(client, file):
+def aggregate_opml(client, file, categs):
     """
     Aggregate all links from an OPML file
     """
@@ -134,9 +147,9 @@ def aggregate_opml(client, file):
         return
 
     log.info('Loading from file: %s' % file)
-    pool = ThreadPool(50, thread_init=attach_connection)
+    pool = ThreadPool(20, thread_init=attach_connection)
     for element in loader:
-        pool.queueTask(lambda worker, p:aggregate_feed(worker.hbase, *p), (element.xmlUrl, ))
+        pool.queueTask(lambda worker, p:aggregate_feed(worker.hbase, *p), (element.xmlUrl, categs))
     pool.joinAll()
 
 def aggregate_feed(client, feed, categs=""):
